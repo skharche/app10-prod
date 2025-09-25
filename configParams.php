d64c9e767da42b479f74aec27bdda0f4d7a1eb1e
Swapnil <swapnil.k00@gmail.com>
2025-09-25 16:26:17 +0530

    Initial Commit- Version 1.0

---
+++ configParams.php
@@ -0,0 +1,11 @@
+<?php
+	$config = array();
+	
+	//Database details
+	$config["dbhost"] = "visgrid.com";
+	$config["dbport"] = "3306";
+	$config["dbname"] = "visgrid_prod";
+	$config["dbuser"] = "production1";
+	$config["dbpassword"] = "jpXfPHTK2C3JdAR";
+	
+?>
