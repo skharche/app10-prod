8ce6e51dca0512dbebbe74b2e7d4832b53f8654e
Swapnil <swapnil.k00@gmail.com>
2026-09-06 22:28:42 -0400

    ignoring file

--- configParams.php
+++ configParams.php
@@ -5,6 +5,8 @@
 	$config["dbhost"] = "floorplan.city";
 	$config["dbport"] = "3306";
 	$config["dbname"] = "floorplan02citydev";
+	
+	 
 	$config["dbuser"] = "floorplan_dev";
 	$config["dbpassword"] = "LM0gT(Mi.E*E";
 	
